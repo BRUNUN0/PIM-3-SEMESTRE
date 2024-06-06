a77355440a722a0fcd65ec4436d435945f315052
Bruno Henrique de Lima Blois <129326520+BRUNUN0@users.noreply.github.com>
2024-06-06 14:18:24 -0300

    Incompatibilidade de navegador
    Resolver incompatibilidade de navegador do google chrome.
    Erro localizado na linha 28 no teste.js, verificar.

--- Development/teste.js
+++ Development/teste.js
@@ -1,25 +1,25 @@
-//  // Função para definir o botão ativo
-//     function setActiveButton() {
-//         // Obtenha o caminho do arquivo atual
-//         const path = window.location.pathname;
-//         const currentPage = path.split("/").pop();
+ // Função para definir o botão ativo
+    function setActiveButton() {
+        // Obtenha o caminho do arquivo atual
+        const path = window.location.pathname;
+        const currentPage = path.split("/").pop();
 
-//         // Defina um mapeamento das páginas para os botões
-//         const pageToButtonMap = {
-//             'home.html': 'botao-home',
-//             'plantacao.html': 'botao-plantacao',
-//             'consumo.html': 'botao-consumo',
-//             'atividades.html': 'botao-atividades'
-//         };
+        // Defina um mapeamento das páginas para os botões
+        const pageToButtonMap = {
+            'home.html': 'botao-home',
+            'plantacao.html': 'botao-plantacao',
+            'consumo.html': 'botao-consumo',
+            'atividades.html': 'botao-atividades'
+        };
 
-//         // Obtenha o ID do botão correspondente à página atual
-//         const activeButtonClass = pageToButtonMap[currentPage];
+        // Obtenha o ID do botão correspondente à página atual
+        const activeButtonClass = pageToButtonMap[currentPage];
 
-//         // Adicione a classe 'active' ao botão correspondente
-//         if (activeButtonClass) {
-//             document.querySelector(`.${activeButtonClass}`).classList.add('active');
-//      }
-// }
+        // Adicione a classe 'active' ao botão correspondente
+        if (activeButtonClass) {
+            document.querySelector(`.${activeButtonClass}`).classList.add('active');
+     }
+}
 
 // Execute a função ao carregar a página
 window.onload = setActiveButton;
@@ -28,11 +28,3 @@ window.onload = setActiveButton;
 document.querySelector('.botao-entrar').addEventListener('click', function() {
     window.location.href = 'home.html';
     });
-
-document.querySelector('.botao-home').addEventListener('click', function() {
-    window.location.href = 'home.html';
-    });
-
-    document.querySelector('.botao-plantacao').addEventListener('click', function() {
-        window.location.href = 'plantacao.html';
-        });
