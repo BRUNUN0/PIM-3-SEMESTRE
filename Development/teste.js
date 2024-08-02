1f8e483634763e3e0df990db868f7ca658ac6ebc
Bruno Henrique de Lima Blois <129326520+BRUNUN0@users.noreply.github.com>
2024-08-02 15:15:07 -0300

    Revert "Ajustes no Pop-Up minha conta"
    This reverts commit dd03d9b1c7f5e61c3576e78bdb1716acdd94d858.

--- Development/teste.js
+++ Development/teste.js
@@ -35,27 +35,7 @@ document.addEventListener('click', fecharMenu);
 
 //Redirecionamento dos Botões do Menu
 
-// Botão "Minha Conta" abre Pop-up
-function abrirPopup(event) {
-    document.getElementById('popup-conta').classList.remove('hidden');
-    event.stopPropagation();
-}
 
-// Esconde Pop-up
-function fecharPopup() {
-    document.getElementById('popup-conta').classList.add('hidden');
-}
-
-// Ativação da função
-document.getElementById('botao-conta').onclick = abrirPopup;
-document.querySelector('.botao-fechar').onclick = fecharPopup;
-
-document.addEventListener('click', function(event) {
-    const popup = document.getElementById('popup-conta');
-    if (!popup.contains(event.target) && !document.getElementById('botao-conta').contains(event.target)) {
-        fecharPopup();
-    }
-});
 // Função Relógio
 function atualizarRelogio() {
     const agora = new Date();
@@ -76,48 +56,3 @@ function atualizarRelogio() {
 
 // Atualiza o relógio a cada segundo
 setInterval(atualizarRelogio, 1);
-
-
-// Analisar
-document.addEventListener("DOMContentLoaded", function() {
-    var popup = document.getElementById("popup-conta");
-    var btn = document.querySelector(".foto-perfil");
-    var span = document.getElementsByClassName("botao-fechar")[0];
-
-    btn.onclick = function() {
-        fetchUserData();
-        popup.classList.remove("hidden");
-    }
-
-    span.onclick = function() {
-        popup.classList.add("hidden");
-    }
-
-    window.onclick = function(event) {
-        if (event.target == popup) {
-            popup.classList.add("hidden");
-        }
-    }
-
-    function fetchUserData() {
-        const data = {
-            id: '12345',
-            nome: 'João da Silva',
-            email: 'joao.silva@gmail.com',
-            dataNascimento: '1990-01-01',
-            telefone1: '123456789',
-            telefone2: '987654321'
-        };
-        
-        preencherDados(data);
-    }
-
-    function preencherDados(data) {
-        document.getElementById('id').value = data.id;
-        document.getElementById('nome').value = data.nome;
-        document.getElementById('email').value = data.email;
-        document.getElementById('data-nascimento').value = data.dataNascimento;
-        document.getElementById('telefone1').value = data.telefone1;
-        document.getElementById('telefone2').value = data.telefone2;
-    }
-});
