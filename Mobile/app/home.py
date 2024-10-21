17f5768042373021877ab3dc8d9e7a48f76a20da
Bruno Henrique de Lima Blois <129326520+BRUNUN0@users.noreply.github.com>
2024-10-21 13:23:06 -0300

    Inicio do desenvolvimento da tela home
    Iniciado o desenvolvimento da tela home, já foi incluso a AppBar.
    É necessário verificar o tema e garantir que os itens da lista do menu estejam na cor preto.

--- Mobile/app/home.py
+++ Mobile/app/home.py
@@ -2,6 +2,67 @@ import flet as ft
 
 def Home(page: ft.Page):
     
+    def logo():
+        logo = ft.Container(
+            content=ft.Image(
+                src="app/assets/Logo.png",
+                width=50,
+                height=50,
+            ),
+            alignment=ft.alignment.top_center,
+        )
+        return logo
+    
+    def bar():
+        appbar = ft.Container(
+            # bgcolor=ft.colors.WHITE,
+            width=390,
+            height=50,
+            padding=ft.padding.only(top=0 ,right=10, left=10),
+            theme=ft.Theme(color_scheme=ft.ColorScheme(primary=ft.colors.BLACK)),
+            content=ft.Row(
+                controls=[
+                    # ft.IconButton(icon=ft.icons.HOME),
+                    logo(), # Chama a logo dentro da row
+                    
+                    # Adiciona o menu de opções
+                    ft.PopupMenuButton(
+                        icon = ft.icons.MENU,
+                        icon_color=ft.colors.BLACK,
+                        menu_position=ft.PopupMenuPosition.UNDER,
+                        
+                        # Items do menu
+                        items=[
+                            ft.PopupMenuItem(
+                                icon=ft.icons.DESCRIPTION,
+                                text='Meus dados',
+                                ),
+                            ft.PopupMenuItem(
+                                icon=ft.icons.LOGOUT,
+                                text='Sair'
+                                )
+                        ]
+                    )
+                ],
+                alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+            )
+        )
+        
+        return appbar
+    
+    def clima():
+        clima = ft.Container(
+            bgcolor=ft.colors.WHITE,
+            width=360,
+            height=160,
+            border_radius=16,
+            padding=ft.padding.only(left=10, right=10),
+        )
+        
+        return clima
+    
+    
+    
     Main = ft.Container(
         width=390,
         height=860,
@@ -11,14 +72,22 @@ def Home(page: ft.Page):
         content=ft.Column(
             controls=[
                 ft.Container(
+                    padding=ft.padding.only(top=5),
                     content=ft.Stack(
                         controls=[
-                            ...
+                            bar()
+                        ]
+                    )
+                ),
+                ft.Container(
+                    padding=ft.padding.only(top=5),
+                    content=ft.Stack(
+                        controls=[
+                            clima()
                         ]
                     )
                 )
             ],
-            alignment=ft.MainAxisAlignment.CENTER,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         )
     )
--- Mobile/app/login.py
+++ Mobile/app/login.py
@@ -4,7 +4,7 @@ def Login(page: ft.Page):
     
     def login(e):
         print("Login Solicitado")
-        page.go('/home')
+        page.go('/')
         
     def rec_senha(e):
         print("Esqueci minha senha, socorro!")
@@ -99,39 +99,6 @@ def Login(page: ft.Page):
                     )
                 ]
             )
-            # ft.Column(
-            # controls=[
-            #     ft.TextField(
-            #         label="ID",
-            #         width=300,
-            #         height=50,
-            #         border_radius=ft.border_radius.all(8),
-            #         bgcolor=ft.colors.WHITE,
-            #         color=ft.colors.BLACK,
-            #         text_style=ft.TextStyle(size=16),
-            #         label_style=ft.TextStyle(
-            #             color=ft.colors.BLACK,
-            #             size=14
-            #         )
-            #     ),
-            #     ft.TextField(
-            #         label="Senha",
-            #         width=300,
-            #         height=50,
-            #         color=ft.colors.BLACK,
-            #         text_style=ft.TextStyle(size=16),
-            #         label_style=ft.TextStyle(
-            #             color=ft.colors.BLACK,
-            #             size=14
-            #         )
-            #     ),
-            #     ft.Row(
-            #         controls=[
-            #             botao()
-            #         ]
-            #     )
-            # ],
-            # horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         )
         return campos
         
@@ -157,110 +124,3 @@ def Login(page: ft.Page):
     )
     
     return Main
-    
-    # ft.Container(
-    #     expand=True,
-    #     content=ft.Stack(
-    #         controls=[
-    #             # Background
-    #             ft.Container(
-    #                 image_src="Desktop/app/assets/Logo.png",
-    #                 image_fit=ft.ImageFit.COVER,
-    #                 expand=True  # Preenche todo o espaço
-    #             ),
-    #             ft.Container(
-    #                 bgcolor=ft.colors.with_opacity(0.4, ft.colors.BLACK),
-    #             ),
-
-    #             # Container de Login
-    #             ft.Column(
-    #                 controls=[
-    #                     ft.Container(
-    #                         bgcolor="#7FA677",
-    #                         width=350,
-    #                         height=300,
-    #                         alignment=ft.alignment.center,
-    #                         border_radius=ft.border_radius.all(10),
-    #                         content=ft.Column(
-    #                             controls=[
-    #                                 ft.Container(
-    #                                     content=ft.Image(
-    #                                         src="Desktop/app/assets/Logo.png",
-    #                                         width=100,
-    #                                         height=100,
-    #                                         fit=ft.ImageFit.CONTAIN,
-    #                                     ),
-    #                                     width=130,
-    #                                     height=130,
-    #                                     alignment=ft.alignment.top_center,
-    #                                     # padding=ft.padding(top=-60),
-    #                                     border_radius=ft.border_radius.all(100),
-    #                                     bgcolor="#7FA677"
-    #                                 ),
-    #                                 # Campo ID
-    #                                 ft.TextField(
-    #                                     label="ID",
-    #                                     width=250,
-    #                                     height=50,
-    #                                     border_radius=ft.border_radius.all(8),
-    #                                     bgcolor=ft.colors.WHITE,
-    #                                     color=ft.colors.BLACK,
-    #                                     text_style=ft.TextStyle(size=16),
-    #                                     label_style=ft.TextStyle(
-    #                                         color=ft.colors.BLACK,
-    #                                         size=14
-    #                                     )
-    #                                 ),
-    #                                 # Campo Senha
-    #                                 ft.TextField(
-    #                                     label="Senha",
-    #                                     width=250,
-    #                                     height=50,
-    #                                     password=True,
-    #                                     border_radius=ft.border_radius.all(8),
-    #                                     bgcolor=ft.colors.WHITE,
-    #                                     color=ft.colors.BLACK,
-    #                                     text_style=ft.TextStyle(size=16),
-    #                                     label_style=ft.TextStyle(
-    #                                         color=ft.colors.BLACK,
-    #                                         size=14
-    #                                     )
-    #                                 ),
-    #                                 # Botão Entrar
-    #                                 ft.ElevatedButton(
-    #                                     text="Entrar",
-    #                                     width=100,
-    #                                     height=40,
-    #                                     bgcolor=ft.colors.GREEN_900,
-    #                                     color=ft.colors.WHITE,
-    #                                     on_click=lambda e: print("Login clicado")
-    #                                 )
-    #                             ],
-    #                             alignment=ft.MainAxisAlignment.CENTER,
-    #                             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-    #                             spacing=20
-    #                         )
-    #                     ),
-    #                 ],
-    #                 alignment=ft.MainAxisAlignment.CENTER,
-    #                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-    #                 expand=True  # Para centralizar o container de login
-    #             ),
-
-    #             # Logo
-    #             # ft.Container(
-    #             #     content=ft.Image(
-    #             #         src="app/assets/logo.png",
-    #             #         fit=ft.ImageFit.CONTAIN
-    #             #     ),
-    #             #     top=120,
-    #             #     width=150,
-    #             #     height=150,
-    #             #     border_radius=ft.border_radius.all(100),
-    #             #     bgcolor="#7FA677",
-    #             # )
-    #         ],
-    #         alignment=ft.alignment.center  # Centraliza todos os itens no Stack
-    #     ),
-    #     alignment=ft.alignment.center
-    # )
