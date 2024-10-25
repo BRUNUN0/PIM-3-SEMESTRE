129689bcc245671788c145dcd1be38f20dff0803
isaque3araujo <131329545+isaque3araujo@users.noreply.github.com>
2024-10-25 07:30:05 -0300

    Um pouco mais

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def main():
         Vhome()
 
 
-# ===== Login
 
+# ==== Viwer
 
 def Vlogin():
     f = False
@@ -22,23 +22,101 @@ def Vlogin():
 
 
         _ = fc.Login(login, senha)
-        if( _ != False):
+        if( _):
             f = True
             print("+++++ Logado!!! +++ ")
             return f
+        else: ("não foi dessa vez")
+
+
+def vFornecedores():
+    
+    _ = input(""" 
+        [1]Vizualizar Fuornecedores
+        [2]ADD Fornecedores
+        [3]delete Fornecedores
+
+""")
+    if _ == 1:
+        return 
+    elif _ == 2:
+        return 
+    elif _ == 3:
+        return 
+    else:
+        return "Valor inválido"
+#---
+    
+
+
+
+def vFuncionario():
+    
+    t = input(""" 
+        [1]Vizualizar Funcionarios
+        [2]ADD Funcionario
+        [3]delete Funcionario
+
+""")
+    if t == 1:
+        return 
+    elif t == 2:
+        return 
+    elif t == 3:
+        return 
+    else:
+        return "Valor inválido"
+#---
+
+
+
+def vCliente():
+    
+    _ = input(""" 
+        [1]Vizualizar FCliente
+        [2]ADD Cliente
+        [3]delete Cliente
+
+""")
+    if _ == 1:
+        return 
+    elif _ == 2:
+        return 
+    elif _ == 3:
+        return 
+    else:
+        return "Valor inválido"
+
+#---
+
 
 # ====== Home
 
 def Vhome():
 
     r = input(""" 
-        [1]Fornecedores\n
-        [2]Funcionarios\n
+        [1]Fornecedores
+        [2]Funcionarios
         [3]Clientes 
         
  """)
+    if r == 1:
+        return vFornecedores()
+    elif r == 2:
+        return vCliente()
+    elif r == 3:
+        return 
+    else:
+        return "Valor inválido"
 
+        
+        
 
+#====
+
+def deletar_funcionario():
+    cpf = input("CPF: ")
+    return fc.Dbc_Funcionario_Del(cpf)
 
 
 
