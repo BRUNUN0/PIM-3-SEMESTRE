6c0ea0389d724d7cf8a1afc39c2fab124720ea4b
Bruno Henrique de Lima Blois <129326520+BRUNUN0@users.noreply.github.com>
2024-11-12 13:01:44 -0300

    Funcionando o login

--- Desktop/main.py
+++ Desktop/main.py
@@ -8,7 +8,8 @@ def main(page: ft.Page):
     rotas(page)
     print(page.route)
 
-    page.go("/adm/funcionarios")
+    # page.go("/adm/funcionarios")
+    page.go('/login')
     page.update()
 
 if __name__ == "__main__":
